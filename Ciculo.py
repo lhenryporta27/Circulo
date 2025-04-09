ed89b4770f226246f2fb2ba10befada93604ea08
lhenryporta27 <74357127@continental.edu.pe>
2025-04-09 11:19:12 -0500

    Se imprementó la circunferencia

--- Ciculo.py
+++ Ciculo.py
@@ -1,2 +1,10 @@
 class Circulo:
-    pass
+    PI = 3.1416
+    def __init__(self, radio):
+        self.radio = radio
+    def circunferencia(self):
+        return 2 * self.PI * self.radio
+
+    if __name__ == "__main__":
+        instamcia_circulo = Circulo(10)
+        print(f"La circunferencia es: {instamcia_circulo.circunferencia()}")
